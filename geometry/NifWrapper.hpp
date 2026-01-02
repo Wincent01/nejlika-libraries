6cb210ead764123a913cf9a3ad1cc10ef52bbd72
Wincent <wincent.holm@gmail.com>
2026-01-02 14:52:16 +0000

    Keep transform info

--- geometry/NifWrapper.hpp
+++ geometry/NifWrapper.hpp
@@ -27,6 +27,10 @@
 
 struct MeshImport
 {
+    glm::vec3 m_Translation = glm::vec3(0.0f);
+    glm::mat3 m_Rotation = glm::mat3(1.0f);
+    float m_Scale = 1.0f;
+
     std::vector<glm::vec3> m_Vertices;
     std::vector<uint32_t> m_Indices;
     std::vector<glm::vec3> m_Normals;
@@ -45,63 +49,63 @@ class NifWrapper
 public:
     /**
      * @brief Load a NIF file
-     * 
+     *
      * @param reader The reader
      */
     void LoadNif(nejlika::Reader& reader);
 
     /**
      * @brief Export the NIF file
-     * 
+     *
      * @param writer The writer
      */
     void ExportNif(nejlika::Writer& writer);
 
     /**
      * @brief Get the header
-     * 
+     *
      * @return const nejlika::geometry::Header& The header
      */
     const nejlika::geometry::Header& GetHeader() const;
 
     /**
      * @brief Get the header
-     * 
+     *
      * @return nejlika::geometry::Header& The header
      */
     nejlika::geometry::Header& GetHeader();
 
     /**
      * @brief Get the blocks
-     * 
+     *
      * @return const std::vector<nejlika::geometry::Construct*>& The blocks
      */
     const std::vector<nejlika::geometry::Construct*>& GetBlocks() const;
-    
+
     /**
      * @brief Get the blocks
-     * 
+     *
      * @return std::vector<nejlika::geometry::Construct*>& The blocks
      */
     std::vector<nejlika::geometry::Construct*>& GetBlocks();
 
     /**
      * @brief Get the block names
-     * 
+     *
      * @return const std::unordered_map<nejlika::geometry::Construct*, std::string>& The block names
      */
     const std::unordered_map<nejlika::geometry::Construct*, std::string>& GetBlockNames() const;
 
     /**
      * @brief Get the block names
-     * 
+     *
      * @return std::unordered_map<nejlika::geometry::Construct*, std::string>& The block names
      */
     std::unordered_map<nejlika::geometry::Construct*, std::string>& GetBlockNames();
 
     /**
      * @brief Add a block to the NIF
-     * 
+     *
      * @param name The name of the block
      * @return nejlika::geometry::Construct* The block
      */
@@ -109,7 +113,7 @@ public:
 
     /**
      * @brief Get the block index
-     * 
+     *
      * @param block The block
      * @return uint32_t The block index
      */
@@ -117,7 +121,7 @@ public:
 
     /**
      * @brief Get a block by index
-     * 
+     *
      * @param index The index
      * @return nejlika::geometry::Construct* The block
      */
@@ -125,7 +129,7 @@ public:
 
     /**
      * @brief Find a node by name
-     * 
+     *
      * @param name The name
      * @return nejlika::geometry::NiNode* The node
      */
@@ -133,7 +137,7 @@ public:
 
     /**
      * @brief Get a string by index
-     * 
+     *
      * @param index The index
      * @return std::string The string
      */
@@ -141,15 +145,15 @@ public:
 
     /**
      * @brief Get the world transform of a node
-     * 
+     *
      * @param node The node
      * @return glm::mat4 The world transform
      */
     glm::mat4 GetWorldTransform(nejlika::geometry::NiAVObject* node);
-    
+
     /**
      * @brief Get the parent of a node
-     * 
+     *
      * @param node The node
      * @return nejlika::geometry::NiNode* The parent
      */
@@ -157,7 +161,7 @@ public:
 
     /**
      * @brief Set the parent of a node
-     * 
+     *
      * @param node The node
      * @param parent The parent, or nullptr to add to the root
      */
@@ -165,40 +169,35 @@ public:
 
     /**
      * @brief Get the root nodes
-     * 
+     *
      * @return const std::vector<nejlika::geometry::NiNode*> The root nodes
      */
     const std::vector<nejlika::geometry::NiNode*> GetRootNodes() const;
 
     /**
      * @brief Set the root nodes
-     * 
+     *
      * @param nodes The root nodes
      */
     void SetRootNodes(const std::vector<nejlika::geometry::NiNode*>& nodes);
 
     /**
      * @brief Add a block to the NIF
-     * 
+     *
      * @tparam T The type of block
      * @param name The name of the block
      * @return T* The block
      */
-    template<typename T>
-    T* AddBlock(const std::string& name)
-    {
-        return dynamic_cast<T*>(AddBlock(name));
-    }
+    template <typename T> T* AddBlock(const std::string& name) { return dynamic_cast<T*>(AddBlock(name)); }
 
     /**
      * @brief Get a pointer to a block
-     * 
+     *
      * @tparam T The type of block
      * @param block The block
      * @return nejlika::geometry::Pointer<T*> The pointer
      */
-    template<typename T>
-    nejlika::geometry::Pointer<T*> PointerTo(nejlika::geometry::Construct* block) const
+    template <typename T> nejlika::geometry::Pointer<T*> PointerTo(nejlika::geometry::Construct* block) const
     {
         if (block == nullptr)
         {
@@ -209,7 +208,7 @@ public:
         {
             throw std::runtime_error("Block is not of the correct type");
         }
-        
+
         uint32_t index = GetBlockIndex(block);
 
         if (index == 0xFFFFFFFF)
@@ -222,20 +221,16 @@ public:
 
     /**
      * @brief Find a block by pointer
-     * 
+     *
      * @tparam T The type of block
      * @param pointer The pointer
      * @return T* The block
      */
-    template<typename T>
-    T* FindBlock(const nejlika::geometry::Pointer<T*>& pointer) const
-    {
-        return pointer.Query(m_Blocks);
-    }
+    template <typename T> T* FindBlock(const nejlika::geometry::Pointer<T*>& pointer) const { return pointer.Query(m_Blocks); }
 
     /**
      * @brief Get the string index of a string
-     * 
+     *
      * @param str The string
      * @note This will add the string to the string table if it does not exist
      * @return nejlika::geometry::StringIndex The string index
@@ -249,7 +244,7 @@ public:
 
     /**
      * @brief Import a mesh into the NIF
-     * 
+     *
      * @param mesh The mesh
      * @param texture Optional texture
      * @return nejlika::geometry::NiTriShape* The shape
@@ -258,7 +253,7 @@ public:
 
     /**
      * @brief Add a node to the NIF
-     * 
+     *
      * @param name The name of the node
      * @param parent The parent of the node, or nullptr to add to the root
      */
@@ -266,7 +261,7 @@ public:
 
     /**
      * @brief Set the local transform of a node
-     * 
+     *
      * @param node The node
      * @param position The position
      * @param rotation The rotation
@@ -276,7 +271,7 @@ public:
 
     /**
      * @brief Set the position of a node
-     * 
+     *
      * @param node The node
      * @param position The position
      */
@@ -284,7 +279,7 @@ public:
 
     /**
      * @brief Set the rotation of a node
-     * 
+     *
      * @param node The node
      * @param rotation The rotation
      */
@@ -292,7 +287,7 @@ public:
 
     /**
      * @brief Set the scale of a node
-     * 
+     *
      * @param node The node
      * @param scale The scale
      */
@@ -300,7 +295,7 @@ public:
 
     /**
      * @brief Get the position of a node
-     * 
+     *
      * @param node The node
      * @return glm::vec3 The position
      */
@@ -308,7 +303,7 @@ public:
 
     /**
      * @brief Get the rotation of a node
-     * 
+     *
      * @param node The node
      * @return glm::quat The rotation
      */
@@ -316,7 +311,7 @@ public:
 
     /**
      * @brief Get the scale of a node
-     * 
+     *
      * @param node The node
      * @return float The scale
      */
@@ -324,7 +319,7 @@ public:
 
     /**
      * @brief Add a property to a node
-     * 
+     *
      * @param node The node
      * @param property The property
      */
@@ -332,7 +327,7 @@ public:
 
     /**
      * @brief Remove a property from a node
-     * 
+     *
      * @param node The node
      * @param property The property
      */
@@ -346,7 +341,7 @@ public:
 private:
     nejlika::geometry::Header m_Header;
     nejlika::geometry::Footer m_Footer;
-    
+
     std::vector<nejlika::geometry::Construct*> m_Blocks;
     std::unordered_map<nejlika::geometry::Construct*, std::string> m_BlockNames;
 };
